1b8af021e5ae718a8ecf8394381898492de4fcf2
YouRekt <czarnecki.jurko@gmail.com>
2026-04-01 19:00:20 +0200

    hw2: ex.6

--- homeworks/hw2/Solution.hs
+++ homeworks/hw2/Solution.hs
@@ -10,18 +10,18 @@ import Data.Sequence (Seq)
 data Sequence a = Empty | Single a | Append (Sequence a) (Sequence a) deriving (Show, Eq)
 
 instance Functor Sequence where
-    fmap :: (a -> b) -> Sequence a -> Sequence b
-    fmap _ Empty = Empty
-    fmap f (Single e) = Single (f e)
-    fmap f (Append l r) = Append (fmap f l) (fmap f r)
+  fmap :: (a -> b) -> Sequence a -> Sequence b
+  fmap _ Empty = Empty
+  fmap f (Single e) = Single (f e)
+  fmap f (Append l r) = Append (fmap f l) (fmap f r)
 
 -- Ex.2
 
 instance Foldable Sequence where
-    foldMap :: (Monoid m) => (a -> m) -> Sequence a -> m
-    foldMap _ Empty = mempty
-    foldMap f (Single e) = f e
-    foldMap f (Append l r) = foldMap f l <> foldMap f r
+  foldMap :: (Monoid m) => (a -> m) -> Sequence a -> m
+  foldMap _ Empty = mempty
+  foldMap f (Single e) = f e
+  foldMap f (Append l r) = foldMap f l <> foldMap f r
 
 seqToList :: Sequence a -> [a]
 seqToList = toList
@@ -32,14 +32,14 @@ seqLength = length
 -- Ex.3
 
 instance Semigroup (Sequence a) where
-    (<>) :: Sequence a -> Sequence a -> Sequence a
-    Empty <> seq = seq
-    seq <> Empty = seq
-    l <> r = Append l r
+  (<>) :: Sequence a -> Sequence a -> Sequence a
+  Empty <> seq = seq
+  seq <> Empty = seq
+  l <> r = Append l r
 
 instance Monoid (Sequence a) where
-    mempty :: Sequence a
-    mempty = Empty
+  mempty :: Sequence a
+  mempty = Empty
 
 -- Ex.4
 
@@ -49,6 +49,33 @@ tailElem target seq = go [seq]
     go [] = False
     go (Empty : xs) = go xs
     go (Single e : xs)
-        | e == target = True
-        | otherwise = go xs
+      | e == target = True
+      | otherwise = go xs
     go (Append l r : xs) = go (l : r : xs)
+
+-- Ex.5
+
+tailToList :: Sequence a -> [a]
+tailToList seq = go [] [seq]
+  where
+    go acc [] = acc
+    go acc (Empty : xs) = go acc xs
+    go acc (Single e : xs) = go (e : acc) xs
+    go acc (Append l r : xs) = go acc (r : l : xs)
+
+-- Ex.6
+
+data Token = TNum Int | TAdd | TSub | TMul | TDiv deriving (Show, Eq)
+
+tailRPN :: [Token] -> Maybe Int
+tailRPN tokens = go [] tokens
+  where
+    go [result] [] = Just result
+    go _ [] = Nothing
+    go stack (TNum n : ts) = go (n : stack) ts
+    go (r : l : stack) (TAdd : ts) = go (l + r : stack) ts
+    go (r : l : stack) (TSub : ts) = go (l - r : stack) ts
+    go (r : l : stack) (TMul : ts) = go (l * r : stack) ts
+    go (0 : l : stack) (TDiv : ts) = Nothing
+    go (r : l : stack) (TDiv : ts) = go (l `div` r : stack) ts
+    go _ _ = Nothing
