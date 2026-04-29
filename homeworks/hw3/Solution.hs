366f80c68eda58e18a87f6263e73f19959a351ea
YouRekt <czarnecki.jurko@gmail.com>
2026-04-29 02:04:12 +0200

    hw3: ex.1

---
+++ homeworks/hw3/Solution.hs
@@ -0,0 +1,30 @@
+module Solution () where
+import Data.Map (Map)
+import qualified Data.Map as Map
+import GHC.Internal.TH.Lib (safe)
+import Data.Text.Array (new)
+
+-- Ex.1
+
+type Pos = (Int, Int)
+data Dir = N | S | E | W deriving (Eq, Ord, Show)
+type Maze = Map Pos (Map Dir Pos)
+
+move :: Maze -> Pos -> Dir -> Maybe Pos
+move maze pos dir = do
+    dirsFromCurrentPos <- Map.lookup pos maze
+    Map.lookup dir dirsFromCurrentPos
+
+followPath :: Maze -> Pos -> [Dir] -> Maybe Pos
+followPath _ pos [] = return pos
+followPath maze pos (d:ds) = do
+    newPos <- move maze pos d
+    followPath maze newPos ds
+
+safePath :: Maze -> Pos -> [Dir] -> Maybe [Pos]
+safePath _ pos [] = return [pos]
+safePath maze pos (d:ds) = do
+    newPos <- move maze pos d
+    safePathTail <- safePath maze newPos ds
+
+    return (newPos:safePathTail)
