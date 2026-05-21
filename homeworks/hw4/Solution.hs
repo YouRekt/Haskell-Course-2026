fe1316365ca39e3ba97b4901360005b6f0c65086
YouRekt <czarnecki.jurko@gmail.com>
2026-05-21 04:02:22 +0200

    hw4: part3

--- homeworks/hw4/Solution.hs
+++ homeworks/hw4/Solution.hs
@@ -24,3 +24,38 @@ asks = Reader
 
 local :: (r -> r) -> Reader r a -> Reader r a
 local f (Reader a) = Reader (a . f)
+
+-- Ex.3
+
+data BankConfig = BankConfig
+  { interestRate :: Double
+  , transactionFee :: Int
+  , minimumBalance :: Int
+  } deriving (Show)
+
+data Account = Account
+  { accountId :: String
+  , balance :: Int
+  } deriving (Show)
+
+calculateInterest :: Account -> Reader BankConfig Int
+calculateInterest account = do
+    config <- ask
+    return $ round $ fromIntegral (balance account) * interestRate config
+
+applyTransactionFee :: Account -> Reader BankConfig Account
+applyTransactionFee account = do
+    config <- ask
+    return $ account { balance = balance account - transactionFee config }
+
+checkMinimumBalance :: Account -> Reader BankConfig Bool
+checkMinimumBalance account = do
+    config <- ask
+    return $ balance account >= minimumBalance config
+
+processAccount :: Account -> Reader BankConfig (Account, Int, Bool)
+processAccount account = do
+    account' <- applyTransactionFee account
+    interest <- calculateInterest account
+    minimumBalance <- checkMinimumBalance account
+    return (account', interest, minimumBalance)
