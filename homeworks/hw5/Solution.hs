54ef7f72187986ba86bc7f030bd45ea4cb2405ec
YouRekt <czarnecki.jurko@gmail.com>
2026-05-21 05:23:15 +0200

    hw5: ex4, ex5, ex6

--- homeworks/hw5/Solution.hs
+++ homeworks/hw5/Solution.hs
@@ -1,8 +1,8 @@
 module Solution () where
 import Control.Monad.State
-    ( MonadState(get), modify, evalState, execState, State )
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Text.Read (readMaybe)
 
 -- Ex.1
 
@@ -96,3 +96,133 @@ editDistM xs ys i j = do
 
 editDistance :: String -> String -> Int
 editDistance xs ys = evalState (editDistM xs ys (length xs) (length ys)) Map.empty
+
+-- Ex.4 Ex.5 Ex.6
+
+data LocationType
+    = Empty
+    | Obstacle Int
+    | Treasure Int
+    | Trap Int
+    | Goal
+    deriving Show
+
+data Location = Location
+    { locName :: String
+    , locType :: LocationType
+    , locNext :: [Int]
+    } deriving Show
+
+data GameState = GameState
+    { position :: Int
+    , energy :: Int
+    , score :: Int
+    , board :: Map Int Location
+    } deriving Show
+
+type AdventureGame a = StateT GameState IO a
+
+getDiceRoll :: IO Int
+getDiceRoll = do
+    putStr "Roll the die (1-6): "
+    line <- getLine
+    case readMaybe line of
+        Just n | n >= 1 && n <= 6 -> return n
+        _ -> putStrLn "Please enter a number between 1 and 6." >> getDiceRoll
+
+getPlayerChoice :: [String] -> IO String
+getPlayerChoice opts = do
+    putStrLn "Choose a path:"
+    mapM_ (\(i, o) -> putStrLn $ "  " ++ show i ++ ") " ++ o) (zip [1 :: Int ..] opts)
+    putStr "Your choice: "
+    line <- getLine
+    case readMaybe line of
+        Just n | n >= 1 && n <= length opts -> return (opts !! (n - 1))
+        _ -> putStrLn "Invalid choice, try again." >> getPlayerChoice opts
+
+displayGameState :: GameState -> IO ()
+displayGameState st = do
+    putStrLn "========================================"
+    let loc = Map.lookup (position st) (board st)
+    putStrLn $ "Location : " ++ maybe "???" locName loc ++ " (#" ++ show (position st) ++ ")"
+    putStrLn $ "Energy   : " ++ show (energy st)
+    putStrLn $ "Score    : " ++ show (score st)
+    putStrLn "========================================"
+
+movePlayer :: Int -> AdventureGame Int
+movePlayer roll = do
+    gameState <- get
+    let walk 0 pos acc = (pos, acc)
+        walk n pos acc = case Map.lookup pos (board gameState) of
+            Nothing  -> (pos, acc)
+            Just location -> case locType location of
+                Goal -> (pos, acc)
+                _ -> case locNext location of
+                    [next] -> walk (n - 1) next (acc + 1)
+                    _ -> (pos, acc)
+    let (newPos, moved) = walk roll (position gameState) 0
+    put gameState { position = newPos }
+    liftIO $ putStrLn $ "You move " ++ show moved ++ " space(s)."
+    return moved
+
+makeDecision :: [String] -> AdventureGame String
+makeDecision options = do
+    liftIO $ putStrLn "A choice lies before you..."
+    liftIO $ getPlayerChoice options
+
+handleLocation :: AdventureGame Bool
+handleLocation = do
+    gameState <- get
+    case Map.lookup (position gameState) (board gameState) of
+        Nothing -> return False
+        Just location -> do
+            liftIO $ putStrLn $ ">> " ++ locName location
+            case locType location of
+                Empty -> return ()
+                Obstacle n -> do
+                    liftIO $ putStrLn $ "An obstacle pushes you back " ++ show n ++ " space(s)!"
+                    modify $ \s -> s { position = max 0 (position s - n) }
+                Treasure n -> do
+                    liftIO $ putStrLn $ "Treasure! +" ++ show n ++ " points."
+                    modify $ \s -> s { score = score s + n }
+                Trap n -> do
+                    liftIO $ putStrLn $ "A trap! -" ++ show n ++ " points."
+                    modify $ \s -> s { score = max 0 (score s - n) }
+                Goal -> liftIO $ putStrLn "You have found the treasure!"
+            case locType location of
+                Goal -> return True
+                _ -> case locNext location of
+                    (_:_:_) -> do
+                        let labels = [ maybe ("#" ++ show p) locName (Map.lookup p (board gameState)) | p <- locNext location ]
+                        choice <- makeDecision labels
+                        let nextPos = head [ p | (p, l) <- zip (locNext location) labels, l == choice ]
+                        modify $ \s -> s { position = nextPos }
+                        return False
+                    _ -> return False
+
+playTurn :: AdventureGame Bool
+playTurn = do
+    gameState <- get
+    liftIO $ displayGameState gameState
+    reached <- handleLocation
+    if reached
+        then return True
+        else do
+            gameState' <- get
+            if energy gameState' <= 0
+                then liftIO (putStrLn "You collapse from exhaustion!") >> return True
+                else do
+                    roll <- liftIO getDiceRoll
+                    _ <- movePlayer roll
+                    modify $ \s -> s { energy = energy s - 1 }
+                    return False
+
+playGame :: AdventureGame ()
+playGame = do
+    ended <- playTurn
+    if ended
+        then do
+            gameState <- get
+            liftIO $ putStrLn "=== GAME OVER ==="
+            liftIO $ displayGameState gameState
+        else playGame
