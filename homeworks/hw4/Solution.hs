8051df8a7530b0154e85e0e5584abd65a8861fac
YouRekt <czarnecki.jurko@gmail.com>
2026-05-21 03:51:59 +0200

    hw4: part2

--- homeworks/hw4/Solution.hs
+++ homeworks/hw4/Solution.hs
@@ -13,3 +13,14 @@ instance Applicative (Reader r) where
 
 instance Monad (Reader r) where
   Reader a >>= f = Reader (\r -> runReader (f (a r)) r)
+
+-- Ex.2
+
+ask :: Reader r r
+ask = Reader id
+
+asks :: (r -> a) -> Reader r a
+asks = Reader
+
+local :: (r -> r) -> Reader r a -> Reader r a
+local f (Reader a) = Reader (a . f)
